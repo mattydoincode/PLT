b6d712d79f2f20665a75a34b9c17ff111359c11a
Alden Quimby <aldenquimby@gmail.com>
2013-10-10 10:55:21 -0400

    fix interpreter spacing

--- microc/interpret.ml
+++ microc/interpret.ml
@@ -21,21 +21,21 @@ let run (vars, funcs) =
 
     (* Evaluate an expression and return (value, updated environment) *)
     let rec eval env = function
-	Literal(i) -> i, env
+	    Literal(i) -> i, env
       | Noexpr -> 1, env (* must be non-zero for the for loop predicate *)
       | Id(var) ->
-	  let locals, globals = env in
-	  if NameMap.mem var locals then
-	    (NameMap.find var locals), env
-	  else if NameMap.mem var globals then
-	    (NameMap.find var globals), env
-	  else raise (Failure ("undeclared identifier " ^ var))
+		  let locals, globals = env in
+		  if NameMap.mem var locals then
+		    (NameMap.find var locals), env
+		  else if NameMap.mem var globals then
+		    (NameMap.find var globals), env
+		  else raise (Failure ("undeclared identifier " ^ var))
       | Binop(e1, op, e2) ->
-	  let v1, env = eval env e1 in
-          let v2, env = eval env e2 in
-	  let boolean i = if i then 1 else 0 in
-	  (match op with
-	    Add -> v1 + v2
+		  let v1, env = eval env e1 in
+	          let v2, env = eval env e2 in
+		  let boolean i = if i then 1 else 0 in
+		  (match op with
+		    Add -> v1 + v2
 	  | Sub -> v1 - v2
 	  | Mult -> v1 * v2
 	  | Div -> v1 / v2
@@ -46,25 +46,25 @@ let run (vars, funcs) =
 	  | Greater -> boolean (v1 > v2)
 	  | Geq -> boolean (v1 >= v2)), env
       | Assign(var, e) ->
-	  let v, (locals, globals) = eval env e in
-	  if NameMap.mem var locals then
-	    v, (NameMap.add var v locals, globals)
-	  else if NameMap.mem var globals then
-	    v, (locals, NameMap.add var v globals)
-	  else raise (Failure ("undeclared identifier " ^ var))
+		  let v, (locals, globals) = eval env e in
+		  if NameMap.mem var locals then
+		    v, (NameMap.add var v locals, globals)
+		  else if NameMap.mem var globals then
+		    v, (locals, NameMap.add var v globals)
+		  else raise (Failure ("undeclared identifier " ^ var))
       | Call("print", [e]) ->
-	  let v, env = eval env e in
-	  print_endline (string_of_int v);
-	  0, env
+		  let v, env = eval env e in
+		  print_endline (string_of_int v);
+		  0, env
       | Call(f, actuals) ->
-	  let fdecl =
-	    try NameMap.find f func_decls
-	    with Not_found -> raise (Failure ("undefined function " ^ f))
+		  let fdecl =
+		    try NameMap.find f func_decls
+		    with Not_found -> raise (Failure ("undefined function " ^ f))
 	  in
 	  let actuals, env = List.fold_left
 	      (fun (actuals, env) actual ->
-		let v, env = eval env actual in v :: actuals, env)
-   	      ([], env) (List.rev actuals)
+			let v, env = eval env actual in v :: actuals, env)
+	   	      ([], env) (List.rev actuals)
 	  in
 	  let (locals, globals) = env in
 	  try
@@ -75,29 +75,29 @@ let run (vars, funcs) =
 
     (* Execute a statement and return an updated environment *)
     let rec exec env = function
-	Block(stmts) -> List.fold_left exec env stmts
+		Block(stmts) -> List.fold_left exec env stmts
       | Expr(e) -> let _, env = eval env e in env
       | If(e, s1, s2) ->
-	  let v, env = eval env e in
-	  exec env (if v != 0 then s1 else s2)
+		  let v, env = eval env e in
+		  exec env (if v != 0 then s1 else s2)
       | While(e, s) ->
-	  let rec loop env =
-	    let v, env = eval env e in
-	    if v != 0 then loop (exec env s) else env
-	  in loop env
+		  let rec loop env =
+		    let v, env = eval env e in
+		    if v != 0 then loop (exec env s) else env
+		  in loop env
       | For(e1, e2, e3, s) ->
-	  let _, env = eval env e1 in
-	  let rec loop env =
-	    let v, env = eval env e2 in
-	    if v != 0 then
-	      let _, env = eval (exec env s) e3 in
-	      loop env
-	    else
-	      env
-	  in loop env
+		  let _, env = eval env e1 in
+		  let rec loop env =
+		    let v, env = eval env e2 in
+		    if v != 0 then
+		      let _, env = eval (exec env s) e3 in
+		      loop env
+		    else
+		      env
+		  in loop env
       | Return(e) ->
-	  let v, (locals, globals) = eval env e in
-	  raise (ReturnException(v, globals))
+		  let v, (locals, globals) = eval env e in
+		  raise (ReturnException(v, globals))
     in
 
     (* Enter the function: bind actual values to formal arguments *)
