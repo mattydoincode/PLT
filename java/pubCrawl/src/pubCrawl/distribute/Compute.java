bba7c04640fae79ac371dc3905c85f756b6b259f
Sireesh Gururaja <sireesh.g36@gmail.com>
2013-10-25 19:03:22 -0400

    Code for RMI, not working because of security.
    This code needs to be amended to work with java's security requirements-
    policy files and codebases.

---
+++ java/pubCrawl/src/pubCrawl/distribute/Compute.java
@@ -0,0 +1,13 @@
+package pubCrawl.distribute;
+
+import pubCrawl.core.IPCFunction;
+import pubCrawl.core.PCObject;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface Compute extends Remote{
+    PCObject  callFunction(IPCFunction function, PCObject param) throws RemoteException;
+
+}
+
--- java/pubCrawl/src/pubCrawl/distribute/distributeServer.java
+++ java/pubCrawl/src/pubCrawl/distribute/distributeServer.java
@@ -1,85 +1,57 @@
 package pubCrawl.distribute;
 
 import pubCrawl.core.IPCFunction;
-import pubCrawl.core.PCList;
 import pubCrawl.core.PCObject;
-
-import java.io.IOException;
-import java.net.UnknownHostException;
-
-import java.net.Socket;
-import java.util.ArrayList;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.Iterator;
+import pubCrawl.distribute.Compute;
 
 
-public class distributeServer {
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
-    private ArrayList<ObjectOutputStream> outStream_list;
-    private ArrayList<ObjectInputStream> inStream_list;
 
+public class distributeServer implements Compute{
 
-    public distributeServer(String[] hosts) throws IOException{
-        connectToSlaves(hosts);
-
+    public distributeServer(){
+        super();
     }
 
-    /**
-     * A method that connects to a list of hosts provided
-     *
-     * @param hosts An array of the hosts to connect to, in String form
-     * @throws IOException Thrown if the machine has a problem connnecting the sockets
-     */
-    public void connectToSlaves(String[] hosts) throws  IOException{
-         Socket tempSlave;
-
-        for(int i = 0;i<hosts.length;i++){
-            tempSlave = new Socket(hosts[i], 1337);
-            outStream_list.add(new ObjectOutputStream(tempSlave.getOutputStream()));
-            inStream_list.add( new ObjectInputStream(tempSlave.getInputStream()));
-        }
+    public PCObject callFunction(IPCFunction function, PCObject param){
+        return function.call(param);
     }
 
-    /**
-     *
-     * @param function
-     * @param toDistribute
-     * @return
-     */
-    public PCList distribute(IPCFunction function, PCList toDistribute) throws IOException , ClassNotFoundException {
+    public static void main(String[] args){
 
-        Iterator<ObjectOutputStream> outStreams = outStream_list.iterator();
-        Iterator<ObjectInputStream> inStreams = inStream_list.iterator();
-        ObjectOutputStream tempSlave_out;
-        ObjectInputStream tempSlave_in;
-        PCObject tempObject;
 
-        for(int i = 0; i<toDistribute.size();i++){
-            if(!outStreams.hasNext())
-                outStreams = outStream_list.iterator();
+        System.setProperty("java.security.policy", "file:///home/sireesh/jsp.policy");
+        System.setProperty("java.rmi.server.codebase", "file:///home/sireesh/PLT/pubCrawl/");
+        System.setProperty("java.rmi.server.hostname", "localhost");
 
-            tempSlave_out = outStreams.next();
-            tempObject = toDistribute.get(i);
-
-            //NEED A THING TO CHECK METHOD SIGNATURE
-            tempSlave_out.writeUTF("function_name");
-            tempSlave_out.writeObject(tempObject);
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
         }
 
-        PCList toReturn = new PCList();
 
-        for(int i = 0; i<toDistribute.size();i++){
-            if(!inStreams.hasNext())
-                inStreams = inStream_list.iterator();
 
-            tempSlave_in = inStreams.next();
 
-            tempSlave_in.readUTF();
-            toReturn.add((PCObject)tempSlave_in.readObject());
+        try {
+            String name = "Compute";
+
+            Compute engine = new distributeServer();
+            Compute stub =
+                    (Compute) UnicastRemoteObject.exportObject(engine, 0);
+
+            Registry registry = LocateRegistry.createRegistry(1099);
+
+            registry.rebind(name, stub);
+
+            System.out.println("ComputeEngine bound");
+        } catch (Exception e) {
+            System.err.println("ComputeEngine exception:");
+            e.printStackTrace();
         }
-
-        return toReturn;
-
     }
 }
+
+
