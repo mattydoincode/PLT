03bbd64433dc2bbf8b739afa6e06a23d228690fd
Sireesh Gururaja <sireesh.g36@gmail.com>
2013-10-31 17:18:44 -0400

    Distribute working in this directory.
    Distribute code is working from within the same directory. Working on
    getting it to work from two separate directories.

---
+++ java/pubCrawl/src/pubCrawl/distributeServer/distributeServer.java
@@ -0,0 +1,53 @@
+package pubCrawl.distributeServer;
+
+import pubCrawl.core.IPCFunction;
+import pubCrawl.core.PCObject;
+import pubCrawl.core.Compute;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+
+public class distributeServer implements Compute {
+
+    public distributeServer(){
+        super();
+    }
+
+    public PCObject callFunction(IPCFunction function, PCObject param){
+        return function.call(param);
+    }
+
+    public static void main(String[] args){
+
+
+        System.setProperty("java.security.policy", "file:///home/sireesh/jsp.policy");
+
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+
+
+
+
+        try {
+            String name = "Compute";
+
+            Compute engine = new distributeServer();
+            Compute stub =
+                    (Compute) UnicastRemoteObject.exportObject(engine, 0);
+
+            Registry registry = LocateRegistry.createRegistry(Integer.parseInt(args[0]));
+
+            registry.rebind(name, stub);
+
+            System.out.println("ComputeEngine bound");
+        } catch (Exception e) {
+            System.err.println("ComputeEngine exception:");
+            e.printStackTrace();
+        }
+    }
+}
+
+
