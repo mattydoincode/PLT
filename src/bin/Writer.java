cf762c2c85b6b40d69957cac0b8fa97b355ec703
Matt Dean <matt.thomas.dean@gmail.com>
2013-11-26 22:20:12 -0500

    character cast

--- src/bin/Writer.java
+++ src/bin/Writer.java
@@ -5,7 +5,7 @@ public class Writer{
     public static PCList print(PCObject toPrint){
     	PCList mylist = (PCList) toPrint;
         for(Iterator<PCObject> it = mylist.iterator();it.hasNext();){
-            System.out.printf("%c", it.next().getBase());
+            System.out.printf("%c", it.next().<Character>getBase());
         }
 
         return mylist;
