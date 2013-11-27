82720d68a2de31a857457137ec4187443b45629e
Alden Quimby <aldenquimby@gmail.com>
2013-11-26 22:22:44 -0500

    new iterator

--- src/bin/Writer.java
+++ src/bin/Writer.java
@@ -4,8 +4,9 @@ public class Writer{
     
     public static PCList print(PCObject toPrint){
     	PCList mylist = (PCList) toPrint;
-        for(Iterator<PCObject> it = mylist.iterator();it.hasNext();){
-            System.out.printf("%c", it.next().<Character>getBase());
+        for(PCObject obj : mylist)
+        {
+            System.out.printf("%c", obj.<Character>getBase());
         }
 
         return mylist;
