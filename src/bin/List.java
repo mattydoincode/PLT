2112efe579142bcec1c6814642a3e8d710130107
Alden Quimby <aldenquimby@gmail.com>
2013-12-12 00:06:23 -0500

    add comment for sireesh

--- src/bin/List.java
+++ src/bin/List.java
@@ -3,6 +3,18 @@ public class List
 	private static PCObject _obj = new PCObject();
 
 	static {
+
+		// TODO SIREESH
+		// see the LRM (in the final report) for details on any of these
+		/*
+			List add(list, item)
+			List remove(list, idx)
+			int find(list, sublist)
+			List<List> split(list, item)
+			List range(min, max)
+			int length(list)
+		*/
+
 		_obj.set("map", new IPCFunction() {
 			@Override
 			public PCObject call(PCObject... args) {
