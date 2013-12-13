02444657a029ad39ba07f2f3368a52d4379193a7
Sireesh Gururaja <sireesh.g36@gmail.com>
2013-12-13 02:01:42 -0500

    strings no longer allowed to have non-escaped " chars

--- src/bin/List.java
+++ src/bin/List.java
@@ -3,6 +3,35 @@ public class List
 	private static PCObject _obj = new PCObject();
 
 	static {
+
+		_obj.set("length", new IPCFunction(){
+			@Override
+			public PCObject call(PCObject... args){
+				PCList list = (PCList)args[0];
+				return new PCObject(list.size());
+
+			}
+		});
+
+		_obj.set("add", new IPCFunction(){
+			@Override
+			public PCObject call(PCObject... args){
+				PCList list = (PCList)args[0];
+				list.add(args[1]);
+				return list;
+			}
+		});
+
+
+//PROBLEMSPROBLEMSPROBLEMSPROBLEMSPROBLEMSPROBLEMSPROBLEMS
+		_obj.set("remove", new IPCFunction(){
+			@Override
+			public PCObject call(PCObject... args){
+				return null;
+			}
+		});
+
+
 		_obj.set("map", new IPCFunction() {
 			@Override
 			public PCObject call(PCObject... args) {
@@ -30,6 +59,66 @@ public class List
 				return newList;
 			}
 		});
+
+		_obj.set("find", new IPCFunction(){
+			@Override
+			//adapted from algs4.cs.princeton.edu/53substrings
+			public PCObject call(PCObject... args){
+				PCList pat = (PCList)args[1];
+				PCList txt = (PCList)args[0];
+
+				int M = pat.size();
+        		int N = txt.size();
+
+		        for (int i = 0; i <= N - M; i++) {
+		            int j;
+		            for (j = 0; j < M; j++) {
+		                if (txt.get(i+j).equals(pat.get(j)))
+		                    break;
+		            }
+		            if (j == M) return new PCObject(i);            // found at offset i
+		        }
+		        return new PCObject(-1)	;                            // not found
+
+			}
+		});
+
+		_obj.set("split", new IPCFunction(){
+			@Override
+			public PCObject call(PCObject... args){
+				PCList list = (PCList)args[0];
+				PCObject wedge = args[1];
+				PCList output = new PCList();
+				int lBound = 0;
+				int uBound = 0;
+
+				for(PCObject element : list){
+					if(element.equals(wedge)){
+						output.add(list.subList(lBound,uBound));
+						lBound = uBound;
+					}
+					uBound++;
+				}
+				return output;
+
+			}
+		});
+
+		_obj.set("range", new IPCFunction(){
+			@Override
+			public PCObject call(PCObject... args){
+				PCList output = new PCList();
+				int lLimit = args[0].<Integer>getBase();
+				int uLimit = args[1].<Integer>getBase();
+				for(;lLimit<=uLimit;lLimit ++){
+					output.add(new PCObject(lLimit));
+				}
+				return output;
+			}
+
+		});
+
+
 	}
 
     public static <T> T get(String key) {
