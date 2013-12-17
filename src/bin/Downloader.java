5c8a62040ea59a6f43bc38dd0c46eea906d154e5
Kevin Michael Mangan <kmm2256@columbia.edu>
2013-12-16 22:58:15 -0500

    download all as one string

--- src/bin/Downloader.java
+++ src/bin/Downloader.java
@@ -6,7 +6,6 @@ import java.net.*;
 public class Downloader
 {
     public static PCList download(PCList listOfChars){
-        PCList toReturn = new PCList();
         String fileName = new String();
 
         for (PCObject element : listOfChars) {
@@ -18,21 +17,22 @@ public class Downloader
         BufferedReader in = new BufferedReader(
                         new InputStreamReader(
                         myURL.openStream()));
-
+        String result = " ";
         try {
             String line = in.readLine();
             while (line != null) 
             {
-                toReturn.add(new PCList(line));
+                result += line;
                 line = in.readLine();
             }
+            
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } finally {
             in.close();
         }    }
         catch(IOException e){}
-        return toReturn;
+        return new PCList(result);
     }
 }
     
