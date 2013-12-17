7537bcfde122f9ad5e73464d43d02e361fafff8f
Kevin Michael Mangan <kmm2256@columbia.edu>
2013-12-16 21:59:30 -0500

    read one line only

--- src/bin/Reader.java
+++ src/bin/Reader.java
@@ -7,21 +7,17 @@ public class Reader
 
     public static PCList read()
     {
-        PCList toReturn = new PCList();
 
         BufferedReader inStream = new BufferedReader(new InputStreamReader(System.in));
         try {
             String temp;
-            while ((temp = inStream.readLine()) != null)
-            {
-                toReturn.add(new PCList(temp));
+            return inStream.readLine();
             }
-        }
         catch(IOException e) {
             e.printStackTrace();
         }
 
-        return toReturn;
+        return null;
     }
 
     public static PCList readFile(PCList listOfChars)  {
