f23502ea81798ad63bf3b729b32a5d7891c30f57
Sireesh Gururaja <sireesh.g36@gmail.com>
2013-12-17 19:32:37 -0500

    x-platforming script

--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 make cleanJava >/dev/null
 make >/dev/null
@@ -10,7 +10,7 @@ javac *.java >/dev/null
 if [[ "$(ifconfig en0 2>/dev/null)" == *error* ]]; then
 	MY_IP=$(ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}')
 else
-	MY_IP=$(ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}')
+	MY_IP=$(ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}')
 fi
 
 java -Djava.rmi.server.codebase=http://$MY_IP:8782/ output $@
